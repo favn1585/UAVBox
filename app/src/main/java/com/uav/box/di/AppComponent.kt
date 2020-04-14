0e7ab113f954700e18f58e040fdc8a4a3c05ff8e
Andrew Kozak <andrew@sgrouples.com>
2020-04-14 13:15:27 +0200

    [Ussue-1] Implement activity view model

--- app/src/main/java/com/uav/box/di/AppComponent.kt
+++ app/src/main/java/com/uav/box/di/AppComponent.kt
@@ -2,7 +2,7 @@ package com.uav.box.di
 
 import com.uav.box.network.di.NetworkModule
 import com.uav.box.repository.di.RepositoryModuleImplementation
-import com.uav.box.MainActivity
+import com.uav.box.ui.MainActivity
 import com.uav.box.app.App
 import dagger.Component
 import javax.inject.Singleton
---
+++ app/src/main/java/com/uav/box/ui/MainActivityViewModel.kt
@@ -0,0 +1,9 @@
+package com.uav.box.ui
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+
+
+
+}
